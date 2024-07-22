178f494f226557c4e09d0357afa1bc7f26e614d6
ddolboghi <rhdfyd128@gmail.com>
2024-07-22 21:27:57 +0900

    test: contextLoads 테스트 추가

---
+++ src/test/java/site/cleanfree/be_configserver/BeConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package site.cleanfree.be_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
